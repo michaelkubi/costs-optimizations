0bcb21194a8005547540b232e3961492353703e5
michaelkubi <michael.bauer@kubiya.ai>
2024-11-11 07:14:01 -0800

    michael

--- tools/costs/main.py
+++ tools/costs/main.py
@@ -18,15 +18,16 @@ def query_prometheus(prometheus_url, query, timeout='30s'):
     Returns:
     - JSON response from Prometheus with the query results
     """
-    url = f"{prometheus_url}/api/v1/query"
-    params = {'query': query, 'timeout': timeout}
+
+    cost_metrics_url = "http://localhost:9090/model/allocation/compute"
+    params = {'window': '7d', 'aggregate': 'namespace', 'includeIdle': 'true', 'step': '1d', 'accumulate': 'true'}
 
     try:
-        response = requests.get(url, params=params)
+        response = requests.get(cost_metrics_url, params=params)
         response.raise_for_status()  # Raise an error for failed requests
         data = response.json()
         if data['status'] == 'success':
-            return data['data']['result']
+            return data
         else:
             raise Exception(f"Query failed with status: {data['status']}")
     except requests.exceptions.RequestException as e:
