7da4c64b5017d015a706210c67a85db53ff13031
michaelkubi <michael.bauer@kubiya.ai>
2024-11-10 19:58:06 -0800

    remove cargo

--- tools/costs/tool_def.py
+++ tools/costs/tool_def.py
@@ -11,18 +11,9 @@ hello_tool = Tool(
     image="python:3.12",
     description="Prints hello {name}!",
     args=[Arg(name="name", description="name to say hello to", required=True)],
-    on_build="""
-curl -LsSf https://astral.sh/uv/install.sh | sh > /dev/null 2>&1
-. $HOME/.cargo/env
-
-uv venv > /dev/null 2>&1
-. .venv/bin/activate > /dev/null 2>&1
-
-if [ -f /tmp/requirements.txt ]; then
-    uv pip install -r /tmp/requirements.txt > /dev/null 2>&1
-fi
-""",
     content="""
+pip install argparse > /dev/null 2>&1
+pip install requests > /dev/null 2>&1
 python /tmp/main.py "{{ .name }}"
 """,
     with_files=[
